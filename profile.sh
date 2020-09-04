01093f647a8ea389e730c4931fbcf0cdaee15cf7
Yufeng <guyufeng0217@163.com>
2020-09-04 22:18:39 +0800

    profile.sh

--- profile.sh
+++ profile.sh
@@ -13,4 +13,9 @@ tar -xf 12345.tar
 
 mkdir training
 
+# Use cProfile to find the most time consuming functions
 python3 -m cProfile -o cProfile.txt clair.py callVarBam --chkpnt_fn ./model --bam_fn testingData/chr21/chr21.bam --ref_fn testingData/chr21/chr21.fa --call_fn ./training/chr21.vcf --sampleName HG001 --pysam_for_all_indel_bases --threads 1 --qual 100 --ctgName chr21 --ctgStart 10269870 --ctgEnd 46672937
+
+# Store intermediate data with --store_loaded_mini_match
+python3 clair.py callVarBam --chkpnt_fn ./model --bam_fn testingData/chr21/chr21.bam --ref_fn testingData/chr21/chr21.fa --call_fn ./training/chr21.vcf --sampleName HG001 --pysam_for_all_indel_bases --threads 1 --qual 100 --ctgName chr21 --ctgStart 10269870 --ctgEnd 46672937 --store_loaded_mini_match
+
